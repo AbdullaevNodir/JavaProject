06e40fd23926582d582ce3f7bd514efeef36f52d
Nodir <nabdullaev584@gmail.com>
2023-02-15 23:34:06 +0600

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = a + b;
+        System.out.println("Result: " + c);
     }
 }
